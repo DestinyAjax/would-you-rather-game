717c1e27adabde5a639a7a77aa3f0980f47a6e45
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-05-19 20:38:35 +0100

    Added pages for the application

---
+++ src/pages/add/AddPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AddPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Add Page</h1>
+            </div>
+        );
+    }
+}
+
+export default AddPage;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import AddPage from "./add/AddPage";
+import LeaderboardPage from "./leaderboard/LeaderboardPage";
+import HomePage from "./home/HomePage";
+
+export {
+    AddPage,
+    LeaderboardPage,
+    HomePage
+};
