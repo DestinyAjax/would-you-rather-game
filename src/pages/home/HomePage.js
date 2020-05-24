91af0e6f333f147e66030d3f9f53f2e6517694d2
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-05-25 00:45:24 +0100

    Added get all questions actions

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,30 +1,54 @@
 import React from "react";
+import { connect } from "react-redux";
 import Layout from "../../components/Layout";
 import { Card, CardBody, TabContent, TabPane, Nav, NavItem, NavLink,} from "reactstrap";
+import { getAllQuestions } from "../../store/actions/questionAction";
 
 class HomePage extends React.Component {
+
+    state = {
+        activeTab: '1'
+    };
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        dispatch(getAllQuestions());
+    };
+
+    toggle = (tab) => {
+        if (this.state.activeTab !== tab) {
+            this.setState(() => ({
+                activeTab: tab
+            }));
+        };
+    };
+
     render() {
+        const { questions } = this.props;
+        const { activeTab } = this.state;
+
         return (
             <Layout title="Home">
                 <div className="row">
                     <div className="col-md-3"></div>
                     <div className="col-md-6">
                         <Card>
-                            <CardBody>
+                            <CardBody style={{padding: 0}}>
                                 <Nav className="tab-nav" tabs>
                                     <NavItem className="item">
-                                        <NavLink onClick={() => {}}>
+                                        <NavLink className={`${activeTab === '1' ? 'active' : ''}`} onClick={() => this.toggle('1')}>
                                             Unanswered Questions
                                         </NavLink>
                                     </NavItem>
                                     <NavItem className="item">
-                                        <NavLink onClick={() => {}}>
+                                        <NavLink className={`${activeTab === '2' ? 'active' : ''}`} onClick={() => this.toggle('2')}>
                                             Answered Questions
                                         </NavLink>
                                     </NavItem>
                                 </Nav>
-                                <TabContent  className="tab-content">
+                                <TabContent activeTab={activeTab} className="tab-content">
                                     <TabPane tabId="1">
+                                        <p>Hello</p>
                                     </TabPane>
                                     <TabPane tabId="2">
                                     </TabPane>
@@ -34,9 +58,45 @@ class HomePage extends React.Component {
                     </div>
                     <div className="col-md-3"></div>
                 </div>
+                <style>{`
+                    .tab-nav {
+                        background-color: #fff;
+                    }
+
+                    .tab-nav .item {
+                        width: 50%;
+                        border-right: 1px solid #ccc;
+                    }
+
+                    .tab-nav a {
+                        font-size: 16px;
+                        line-height: 24px;
+                        color: #222222 !important;
+                        padding: 10px;
+                        border: 0;
+                    }
+
+                    .tab-nav a:hover {
+                        background-color: #ccc !important;
+                        font-weight: bold;
+                    }
+    
+                    .tab-nav a.active {
+                        background-color: #ccc !important;
+                        font-weight: bold;
+                    }
+
+                    .tab-content {
+                        padding: 20px;
+                    }
+                `}</style>
             </Layout>
         );
     }
 }
 
-export default HomePage;
+const mapStateTopProps = state => ({
+    questions: state.questions.questions
+});
+
+export default connect(mapStateTopProps)(HomePage);
